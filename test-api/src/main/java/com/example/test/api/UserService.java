0eaf41ac911d212dabb4c51df53e5e4fae63524e
Fan5Shi <963326056he@gmail.com>
2021-01-16 15:27:10 +0800

    创建本地仓库

---
+++ test-api/src/main/java/com/example/test/api/UserService.java
@@ -0,0 +1,14 @@
+package com.example.test.api;
+
+
+import com.example.test.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
